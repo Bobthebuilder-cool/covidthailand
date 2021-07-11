96601f512a09984184797eec5979a41782abdfd4
Victor Djasli Phisitkul <victordjasliphisitkul@Victors-MacBook-Pro.local>
2021-07-11 15:55:02 +0700

    first commit

---
+++ precache-manifest.a636ae10e5b31244c7a03ea00d2ae8e3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97989bdd3ab6dae4ae3e6b0c1a44de38",
+    "url": "/coolstuff/index.html"
+  },
+  {
+    "revision": "93b68c7dd561bb629e3d",
+    "url": "/coolstuff/static/css/main.938687a4.chunk.css"
+  },
+  {
+    "revision": "d7eb300f166f70543654",
+    "url": "/coolstuff/static/js/2.a24248e7.chunk.js"
+  },
+  {
+    "revision": "93b68c7dd561bb629e3d",
+    "url": "/coolstuff/static/js/main.4957abfb.chunk.js"
+  },
+  {
+    "revision": "469d989f74ed2163882a",
+    "url": "/coolstuff/static/js/runtime~main.e05506dd.js"
+  }
+]);
